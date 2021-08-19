8bb2fd0abc57c1597f7fdde35fc81576b068894d
rotemdan267 <rotemdan267@gmail.com>
2021-08-19 22:39:28 +0300

    Add project files.

---
+++ HW-Generics/GenericArr.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Generics
+{
+    class GenericArr<TD>
+    {
+        TD[] arr = new TD[100];
+        bool isReadOnly;
+        public int Count { get; set; } = 0;
+
+        public GenericArr()
+        {
+        }
+
+        public GenericArr(TD[] arr, int count)
+        {  // לא בטוח שזה הכי הגיוני לקבל את ה-count
+            // מבחוץ אבל זה היה לי נוח לבצע בדיקות ככה
+            this.arr = arr;
+            Count = count;
+        }
+
+        public void Add(TD td)
+        {
+            if (!isReadOnly)
+            {
+                arr[Count] = td;
+                Count++;
+            }
+            else
+            {
+                throw new InvalidOperationException("ReadOnly Mode is activated");
+            }
+        }
+        public void Delete(TD td)
+        {
+            if (!isReadOnly)
+            {
+                for (int i = 0; i < Count; i++)
+                {
+                    if (arr[i].Equals(td))
+                    {
+                        arr[i] = default(TD);
+                    }
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException("ReadOnly Mode is activated");
+            }
+        }
+        public void ActivateReadOnlyMode()
+        {
+            isReadOnly = true;
+        }
+        public void CancelReadOnlyMode()
+        {
+            isReadOnly = false;
+        }
+    }
+}
---
+++ HW-Generics/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrr = new int[100];
+            for (int i = 0; i < 10; i++)
+            {
+                arrr[i] = i + 1;
+            }
+            GenericArr<int> arr = new GenericArr<int>(arrr, 10);
+            arr.ActivateReadOnlyMode();
+            try
+            {
+                arr.Add(84);
+            }
+            catch(InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            arr.CancelReadOnlyMode();
+            try
+            {
+                arr.Add(84);
+                arr.Delete(5);
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
